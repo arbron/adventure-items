67d3c210356a1f062942afdab448e221c3d75729
Jeff Hitchcock <mail@command-center.org>
2021-05-25 14:23:39 -0700

    Add yaml as option for storing data, add another GHC adventure

--- Package.swift
+++ Package.swift
@@ -14,12 +14,15 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.6.0")
+        .package(name: "Ink", url: "https://github.com/johnsundell/ink.git", from: "0.5.0"),
+        .package(name: "Plot", url: "https://github.com/johnsundell/plot.git", from: "0.9.0"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0"),
+        .package(name: "Yams", url: "https://github.com/jpsim/Yams.git", from: "4.0.6")
     ],
     targets: [
         .target(
             name: "AdventureItems",
-            dependencies: ["Publish"]
+            dependencies: ["Ink", "Plot", "Publish", "Yams"]
         )
     ]
 )
--- Sources/AdventureItems/main.swift
+++ Sources/AdventureItems/main.swift
@@ -2,6 +2,7 @@ import Foundation
 import Files
 import Publish
 import Plot
+import Yams
 
 // This type acts as the configuration for your website.
 struct AdventureItemsSite: Website {
@@ -24,13 +25,25 @@ struct AdventureItemsSite: Website {
     static let indentationMode: Indentation.Kind = .spaces(2)
 }
 
-let decoder = JSONDecoder()
-decoder.dateDecodingStrategy = .formatted(.iso8601date)
+// JSON Decoding
+let jsonDecoder = JSONDecoder()
+jsonDecoder.dateDecodingStrategy = .formatted(.iso8601date)
 
 let dataFolder = try Folder.packageFolder(path: "Sources/Data/")
-let files = dataFolder.files.compactMap { $0.extension == "json" ? $0 : nil }
+let jsonFiles = dataFolder.files.recursive.compactMap { $0.extension == "json" ? $0 : nil }
 
-let adventures: [Adventure] = try decoder.decode([Adventure].self, files: files)
+var adventures: [Adventure] = try jsonDecoder.decode([Adventure].self, files: jsonFiles)
+
+// YAML Decoding
+let yamlDecoder = YAMLDecoder()
+let yamlFiles = dataFolder.files.recursive.compactMap { $0.extension == "yaml" ? $0 : nil }
+for file in yamlFiles {
+    fputs("Loading Adventures from \(file.name)\n", stdout)
+    adventures.append(contentsOf: try yamlDecoder.decode([Adventure].self, from: file.readAsString()))
+}
+
+adventures.sort { (lhs, rhs) in lhs.code < rhs.code }
+
 
 // This will generate your website using the built-in Foundation theme:
 try AdventureItemsSite().publish(using: [
